4c4f2ebbe9aa814bb4b0655dd2693311b461785d
anthonykh01 <anthonyykh01@gmail.com>
2024-01-22 16:52:18 +0200

    DISPLAY FUNCTION

--- gameoflife.py
+++ gameoflife.py
@@ -2,3 +2,7 @@ import random
 
 def create_grid(width, height):
     return [[random.choice([0, 1]) for _ in range(width)] for _ in range(height)]
+
+def display_grid(grid):
+    for row in grid:
+        print(''.join('#' if cell else ' ' for cell in row))
